8eaf2a90299f086948c61e79911536fa7dd2003c
Skylar-Lorena <lorayne911@gmail.com>
2023-09-12 09:03:44 +0300

    initial commit of project files

---
+++ index.js
@@ -0,0 +1,38 @@
+// JavaScript Expressions
+
+// Variables and Expressions (5 minutes):
+// Exercise: Define two variables, 'x' and 'y', and calculate their sum. Log the result to the console.
+
+// Conditional Statements (5 minutes):
+// Exercise: Write an 'if' statement that checks if 'x' is greater than 'y'. Log the appropriate message to the console.
+
+// Function Basics (5 minutes):
+// Exercise: Create a function named 'greet' that takes a 'name' parameter and logs a greeting message to the console when called.
+
+// Looping (5 minutes):
+// Exercise: Use a 'for' loop to print numbers from 1 to 5 to the console.
+
+// JavaScript Statements
+
+// Programming as Conversation (5 minutes):
+// Exercise: Explain the concept of programming as a conversation between you and the computer in a comment.
+
+// Selection with Conditionals (5 minutes):
+// Exercise: Write an 'if' statement that checks the value of 'score' and logs a corresponding grade to the console.
+
+// Selection with Conditional: the 'switch' Statement (5 minutes):
+// Exercise: Implement a 'switch' statement that evaluates the value of 'day' and logs the corresponding day of the week to the console.
+
+// Logging in JavaScript (5 minutes):
+// Exercise: Use the 'console.log' method to log a message to the console explaining a concept you've learned today.
+
+// JavaScript Functions
+
+// Function Basics (5 minutes):
+// Exercise: Create a function named 'multiply' that takes two parameters, 'a' and 'b', and returns their product.
+
+// Function Parameters (5 minutes):
+// Exercise: Modify the 'greet' function to accept an additional parameter, 'timeOfDay', and customize the greeting based on the time of day.
+
+// JavaScript Calculator Challenge Lab (10 minutes):
+// Exercise: Implement a calculator program in JavaScript that can perform addition, subtraction, multiplication, and division operations based on user input.
